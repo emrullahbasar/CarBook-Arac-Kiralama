3f3736d818ba5e05d9c03819131615e39a383848
emrullahbasar <emrullah-basar@outlook.com>
2025-11-05 13:05:30 +0300

    Add files via upload

---
+++ Infrastructure/CarBook.Persistence/Migrations/20251022102647_mig_add_rezervation_car_relation.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarBook.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_add_rezervation_car_relation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Rezervations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rezervations_CarID",
+                table: "Rezervations",
+                column: "CarID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rezervations_Cars_CarID",
+                table: "Rezervations",
+                column: "CarID",
+                principalTable: "Cars",
+                principalColumn: "CarID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rezervations_Cars_CarID",
+                table: "Rezervations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rezervations_CarID",
+                table: "Rezervations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Rezervations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ Infrastructure/CarBook.Persistence/Migrations/20251022105826_mig_add_status_rezervation.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarBook.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_add_status_rezervation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Rezervations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Rezervations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Rezervations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Rezervations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
